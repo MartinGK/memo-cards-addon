3a4c8e1b74b4ba15f52402f4a56e5ff6933ca272
Martin GK <mgainza@bordo.ai>
2023-11-10 08:41:24 -0300

    feat: first commit with requirements

---
+++ main.js
@@ -0,0 +1,6 @@
+//I need the following features:
+
+//The app:
+// 1) Should be enabled somewhere in the google translate page
+// 2) Should be able to get the string to be translated and the translated string.
+// 3) Should be able to add a card from an endpoint.
